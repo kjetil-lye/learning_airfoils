4774f4cfdb8aa0c4e89fe6f78c23fd0e2ddd5476
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2018-10-18 13:20:37 +0200

    almost perfectly reproduceable now

---
+++ docker/run_airfoils_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+docker build . -t kjetilly/airfoils_learning:git
+repodir=$(dirname $(pwd))
+
+docker run -v $repodir:/project --rm kjetilly/airfoils_learning:git bash -c 'pip3 freeze > /project/notebooks/pip_airfoils_packages.txt'
+docker run -v $repodir:/project --rm kjetilly/airfoils_learning:git bash -c 'cd /project/notebooks && jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute MachineLearningSixParametersAirfoil.ipynb --output MachineLearningSixParametersAirfoilOutput.ipynb'
+
