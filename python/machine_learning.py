f14b410be4af512c4aa4a65431d6b490b897162b
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2018-11-05 15:03:53 +0100

    now supporting different selection criterea

--- python/machine_learning.py
+++ python/machine_learning.py
@@ -164,6 +164,20 @@ def get_network(parameters, data, *, network_information, output_information):
             train_error = np.sum(hist.history['loss'][-min(network_information.error_length,epochs):])
         elif network_information.selection == 'prediction':
             train_error = compute_prediction_error(data, np.reshape(model.predict(parameters), data.shape), train_size, 2)#np.sum(np.linalg.norm(data - np.reshape(model.predict(parameters), data.shape), ord=2))/data.shape[0]
+        elif network_information.selection == 'mean_tail':
+            train_error = abs(np.sum(data)/data.shape[0] - np.sum(np.reshape(model.predict(parameters), data.shape))/data.shape[0])
+        elif network_information.selection == 'mean':
+            qmc_means = []
+            dlqmc_means = []
+
+            for k in range(1, data.shape[0]):
+                qmc_means.append(np.sum(data[:k])/k)
+                dlqmc_means.append(np.sum(np.reshape(model.predict(parameters[:k,:]), data.shape))/k)
+
+            qmc_means = np.array(qmc_means)
+            dlqmc_means = np.array(dlqmc_means)
+
+            train_error = np.sum(abs(qmc_means-dlqmc_means))
         else:
             raise Exception("Unknown selection %s" % network_information.selection)
         if best_network is None or train_error < best_learning_rate:
@@ -651,6 +665,7 @@ def plot_train_size_convergence(network_information,
 
         plt.loglog(train_sizes, error, '-o',label='DLQMC %s' % error_key)
 
+        print("errors[%s] = [%s]" % (error_key, ", ".join(error)))
         if 'prediction' not in error_key:
             comparison_error = errors_comparison[error_key]
             plt.loglog(train_sizes, comparison_error, '-o',label='QMC %s' % error_key)
