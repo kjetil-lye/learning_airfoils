2058f4936e38c7c1894da39911f2855852ddc43f
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-01-28 13:33:47 +0100

    limited output

--- python/plot_info.py
+++ python/plot_info.py
@@ -164,14 +164,29 @@ def console_log_show(x):
         x = "{} (in unknown function): {}".format(str(datetime.datetime.now()), x)
     console_log(x)
     print(x)
-
+def isnotebook():
+    # see  https://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
 def console_log(x):
     """Simple hack to write to stdout from a notebook"""
 
     x=str(x)
-    with open('/dev/stdout', 'w') as f:
-        f.write("DEBUG: %s\n"%x)
-        f.flush()
+
+    if isnotebook():
+        with open('/dev/stdout', 'w') as f:
+            f.write("DEBUG: %s\n"%x)
+            f.flush()
+    else:
+        print(x)
 
 def to_percent(y, position):
     # see https://stackoverflow.com/questions/31357611/format-y-axis-as-percent
