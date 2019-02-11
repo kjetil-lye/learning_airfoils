65b7a217ca923afee168164db0cc45bab60d7d68
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-02-11 10:25:45 +0100

    fixes

--- submit_all.sh
+++ submit_all.sh
@@ -6,21 +6,21 @@
 # EDIT THIS LINE TO THE CORRECT LOCATION
 export AIRFOILS_DLMC_KH_DATAPATH=/cluster/project/sam/klye/kh_airfoils_data/
 
-if [[ ! -d ${AIRFOILS_DLMC_KH_DATAPAPTH} ]];
+if [[ ! -d ${AIRFOILS_DLMC_KH_DATAPATH} ]];
 then
     echo "The exported variable AIRFOILS_DLMC_KH_DATAPATH does not point to an existing folder"
     echo "Please update $0 and set it to a folder containing kh_1.nc and qmc_large.txt"
     exit 1
 fi
 
-if [ ! -f ${AIRFOILS_DLMC_KH_DATAPAPTH}/kh_1.nc ];
+if [ ! -f ${AIRFOILS_DLMC_KH_DATAPATH}/kh_1.nc ];
 then
     echo "The exported variable AIRFOILS_DLMC_KH_DATAPATH does not contain kh_1.nc"
     echo "Please update $0 and set it to a folder containing kh_1.nc and qmc_large.txt"
     exit 1
 fi
 
-if [ ! -f ${AIRFOILS_DLMC_KH_DATAPAPTH}/qmc_large.txt ];
+if [ ! -f ${AIRFOILS_DLMC_KH_DATAPATH}/qmc_large.txt ];
 then
     echo "The exported variable AIRFOILS_DLMC_KH_DATAPATH does not contain qmc_large.txt"
     echo "Please update $0 and set it to a folder containing kh_1.nc and qmc_large.txt"
