05aeb89364c61cb607903458c55cda6fc5e3fb9f
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-02-12 23:15:37 +0100

    made script to run speedtests

---
+++ notebooks/run_speedtests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This corresponds to 128 training samples
+export MACHINE_LEARNING_TRAINING_SIZE=2
+export OMP_NUM_THREADS=1
+set -e
+python3 ../python/split_best_networks_into_individual_files.py
+
+for network in 'Lift' 'Drag' 'effective'
+do
+    python3 ../python/speedtest.py --json_file ../data/${network}_best_network.json --data_source 'Airfoils' --functional_name Lift --functional_name Drag &> log_speedtest_${network}_airfoils.txt;
+done
+
+for network in 'Q1' 'Q2' 'Q3' 'effective'
+do
+  python3 ../python/speedtest.py --json_file ../data/${network}_best_network.json --data_source 'SodShockTubeQMC' --functional_name Q1 --functional_name Q2 --functional_name Q3 &> log_speedtest_${network}_sod.txt;
+done
