66e6a5fee2539c81a2e04c88a513fac4c7bececb
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-02-13 10:36:09 +0100

    added dockerfiles for speedtest

--- docker/run_speedtest_docker.sh
+++ docker/run_speedtest_docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-
-docker build . -t kjetilly/airfoils_learning:git
-repodir=$(dirname $(pwd))
 userid=$(id -u)
+docker build . -t kjetilly/airfoils_learning:git --build-arg USERID=${userid}
+
+repodir=$(dirname $(pwd))
+
 docker run --user ${userid} -v $repodir:/project --rm kjetilly/airfoils_learning:git bash -c 'cd /project/notebooks; bash run_speedtests.sh'
