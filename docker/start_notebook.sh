25f1f32b438ce20d921ffd7425ac4f3c63d58c87
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-02-14 21:54:52 +0100

    fxes to start notebook

--- docker/start_notebook.sh
+++ docker/start_notebook.sh
@@ -6,5 +6,5 @@ docker build . -t kjetilly/airfoils_learning:git --build-arg USERID=${userid}
 
 repodir=$(dirname $(pwd))
 docker run -p 8888:8888 --user ${userid} --rm -v $(dirname $(pwd)):/project \
-       airfoils_learning:git bash -c 'cd /project/notebooks; jupyter notebook --ip 0.0.0.0 --port 8888'
+       kjetilly/airfoils_learning:git bash -c 'cd /project/notebooks; jupyter notebook --ip 0.0.0.0 --port 8888'
 
